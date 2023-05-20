2e3cb14f2e20ef39e5fdb9d1de7f3d72d9933783
xing.heng <enterbacks@gmail.com>
2023-05-20 09:17:07 +0800

    fix: 去掉多余依赖

--- packages/BigScreenManagement/index.vue
+++ packages/BigScreenManagement/index.vue
@@ -235,7 +235,7 @@ import BigScreenRun from 'packages/BigScreenRun/index.vue'
 import Icon from 'packages/assets/images/pageIcon/export'
 import { getPageType } from './utils'
 import _ from 'lodash'
-import axios from 'axios'
+// import axios from 'axios'
 import { post } from 'packages/js/utils/http'
 import IconSvg from 'packages/SvgIcon'
 let dashBoardPageCode = null
