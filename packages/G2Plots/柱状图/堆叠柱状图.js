8853ca46afb0f7c0fecabaf13bec68fc7845a501
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-05-23 11:35:48 +0800

    feat:修复堆叠柱状图显示异常的问题

--- packages/G2Plots/柱状图/堆叠柱状图.js
+++ packages/G2Plots/柱状图/堆叠柱状图.js
@@ -278,16 +278,7 @@ const option = {
     // 配置样式
     style: {
       opacity: 1
-    },
-    // 可配置附加的布局方法
-    layout: [
-      // 柱形图数据标签位置自动调整
-      { type: 'interval-adjust-position' },
-      // 数据标签防遮挡
-      { type: 'interval-hide-overlap' },
-      // 数据标签文颜色自动调整
-      { type: 'adjust-color' }
-    ]
+    }
   },
   yAxis: {
     grid: {
