0b63ca48a695f01371acf88155af13141b2df586
wu.jian2 <distantmtn@gmail.com>
2023-05-25 10:35:57 +0800

    style: loading 样式调整

--- packages/js/utils/updateTheme.js
+++ packages/js/utils/updateTheme.js
@@ -1,11 +1,9 @@
 export default function updateTheme (className) {
   const querySelectorName = className || '.el-button--primary'
-  try {
-    const primaryButton = document.querySelector(querySelectorName)
+  const primaryButton = document.querySelector(querySelectorName)
+  if (primaryButton) {
     const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
     const element = document.querySelector('.bs-body-theme-wrap')
     element.style.setProperty('--bs-el-color-primary', backgroundColor)
-  } catch (e) {
-    console.error(e)
   }
 }
