d95af1781777f91cf27fc779886b957f1c4576ac
wu.jian2 <distantmtn@gmail.com>
2023-05-25 11:25:17 +0800

    fix: 优化更新主题，获取不到dom就执行方法的问题

--- packages/js/utils/updateTheme.js
+++ packages/js/utils/updateTheme.js
@@ -1,11 +1,13 @@
-export default function updateTheme (className) {
-  window.requestAnimationFrame(() => {
-    const querySelectorName = className || '.el-button--primary'
-    const primaryButton = document.querySelector(querySelectorName)
-    if (primaryButton) {
-      const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
-      const element = document.querySelector('.bs-body-theme-wrap')
-      element.style.setProperty('--bs-el-color-primary', backgroundColor)
-    }
-  })
+export default function updateTheme (data) {
+  if (data) {
+    window.requestAnimationFrame(() => {
+      const querySelectorName = data || '.el-button--primary'
+      const primaryButton = document.querySelector(querySelectorName)
+      if (primaryButton) {
+        const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
+        const element = document.querySelector('.bs-body-theme-wrap')
+        element.style.setProperty('--bs-el-color-primary', backgroundColor)
+      }
+    })
+  }
 }
