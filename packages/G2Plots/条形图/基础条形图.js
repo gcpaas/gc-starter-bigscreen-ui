09b9f4c0f7990f0058441888f4caa66d648272e8
hong.yang <hong.yang@ustcinfo.com>
2023-05-25 14:10:47 +0800

    fix: 条形图属性完善
    条形图属性完善

--- packages/G2Plots/条形图/基础条形图.js
+++ packages/G2Plots/条形图/基础条形图.js
@@ -51,6 +51,110 @@ const setting = [
     tabName: 'custom'
   },
   {
+    label: '显示图例',
+    type: 'switch', // 设置组件类型
+    field: 'legendEnable', // 字段
+    optionField: 'legendEnable', // 对应options中的字段
+    value: false,
+    tabName: 'custom'
+  },
+  // {
+  //   label: '图例排列方向',
+  //   type: 'select', // 设置组件类型
+  //   field: 'legendLayout', // 字段
+  //   optionField: 'legendLayout', // 对应options中的字段
+  //   // 是否多选
+  //   multiple: false,
+  //   value: 'horizontal',
+  //   tabName: 'custom',
+  //   options: [
+  //     {
+  //       label: '水平',
+  //       value: 'horizontal'
+  //     },
+  //     {
+  //       label: '垂直',
+  //       value: 'vertical'
+  //     }
+  //   ]
+  // },
+  {
+    label: '图例位置',
+    type: 'select', // 设置组件类型
+    field: 'legendPosition', // 字段
+    optionField: 'legendPosition', // 对应options中的字段
+    // 是否多选
+    multiple: false,
+    value: 'top',
+    tabName: 'custom',
+    options: [
+      {
+        label: '正上方',
+        value: 'top'
+      },
+      {
+        label: '左上方',
+        value: 'top-left'
+      },
+      {
+        label: '右上方',
+        value: 'top-right'
+      },
+      {
+        label: '正左方',
+        value: 'left'
+      },
+      {
+        label: '左上方',
+        value: 'left-top'
+      },
+      {
+        label: '左下方',
+        value: 'left-bottom'
+      },
+      {
+        label: '正右方',
+        value: 'right'
+      },
+      {
+        label: '右上方',
+        value: 'right-top'
+      },
+      {
+        label: '右下方',
+        value: 'right-bottom'
+      },
+      {
+        label: '正下方',
+        value: 'bottom'
+      },
+      {
+        label: '左下方',
+        value: 'bottom-left'
+      },
+      {
+        label: '右下方',
+        value: 'bottom-right'
+      }
+    ]
+  },
+  {
+    label: '网格线颜色',
+    type: 'colorPicker', // 设置组件类型
+    field: 'xAxis_grid_line_style_stroke', // 字段
+    optionField: 'xAxis.grid.line.style.stroke', // 对应options中的字段
+    value: '#d0d0d0',
+    tabName: 'custom'
+  },
+  {
+    label: '网格线宽度',
+    type: 'inputNumber', // 设置组件类型
+    field: 'xAxis_grid_line_style_lineWidth', // 字段
+    optionField: 'xAxis.grid.line.style.lineWidth', // 对应options中的字段
+    value: 0,
+    tabName: 'custom'
+  },
+  {
     label: '颜色配置',
     // 设置组件类型
     type: 'colorSelect',
@@ -81,7 +185,8 @@ const data = [
 ]
 
 // 数据处理脚本
-const dataHandler = 'option.seriesField = setting.find(settingItem=>settingItem.field === \'yField\').value'
+const dataHandler = 'option.seriesField = setting.find(settingItem=>settingItem.field === \'yField\').value' +
+  '\noption.legend = option.legendEnable ? {position: setting.find(settingItem=>settingItem.field === \'legendPosition\').value} : false'
 
 // 图表配置 new Line('domName', option)
 const option = {
@@ -91,13 +196,24 @@ const option = {
   seriesField: 'year',
   color: ['#5B8FF9', '#61DDAA', '#5D7092', '#F6BD16', '#6F5EF9', '#6DC8EC', '#945FB9', '#FF9845', '#1E9493', '#FF99C3'],
   appendPadding: [20, 20, 20, 20], // 设置图标的边距
-  legend: {
-    position: 'top-left'
-  },
+  legendEnable: true,
+  legendLayout: 'vertical',
+  legendPosition: 'top',
+  legend: false,
   yAxis: {
     label: {
       style: {
         opacity: 1
+      }
+    }
+  },
+  xAxis: {
+    grid: {
+      line: {
+        style: {
+          stroke: '#d0d0d0',
+          lineWidth: 0
+        }
       }
     }
   }
