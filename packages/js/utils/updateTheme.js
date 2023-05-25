044424c95bfbaa08e1e94cff930db13bbb1540e9
wu.jian2 <distantmtn@gmail.com>
2023-05-25 10:09:51 +0800

    feat: 完善更新主题方法

--- packages/js/utils/updateTheme.js
+++ packages/js/utils/updateTheme.js
@@ -1,8 +1,11 @@
 export default function updateTheme (className) {
   const querySelectorName = className || '.el-button--primary'
-  const primaryButton = document.querySelector(querySelectorName)
-  // 获取背景色属性值
-  const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
-  const element = document.querySelector('.bs-body-theme-wrap')
-  element.style.setProperty('--bs-el-hover', backgroundColor)
+  try {
+    const primaryButton = document.querySelector(querySelectorName)
+    const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
+    const element = document.querySelector('.bs-body-theme-wrap')
+    element.style.setProperty('--bs-el-hover', backgroundColor)
+  } catch (e) {
+    console.error(e)
+  }
 }
