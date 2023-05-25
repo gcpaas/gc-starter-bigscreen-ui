a0bd2530bb7b5e66012999a566e94696830547e9
wu.jian2 <distantmtn@gmail.com>
2023-05-25 10:50:48 +0800

    fix: 优化更新主题，获取不到dom就执行方法的问题

--- packages/js/utils/updateTheme.js
+++ packages/js/utils/updateTheme.js
@@ -1,9 +1,11 @@
 export default function updateTheme (className) {
-  const querySelectorName = className || '.el-button--primary'
-  const primaryButton = document.querySelector(querySelectorName)
-  if (primaryButton) {
-    const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
-    const element = document.querySelector('.bs-body-theme-wrap')
-    element.style.setProperty('--bs-el-color-primary', backgroundColor)
-  }
+  document.addEventListener('DOMContentLoaded', () => {
+    const querySelectorName = className || '.el-button--primary'
+    const primaryButton = document.querySelector(querySelectorName)
+    if (primaryButton) {
+      const backgroundColor = window.getComputedStyle(primaryButton).getPropertyValue('background-color')
+      const element = document.querySelector('.bs-body-theme-wrap')
+      element.style.setProperty('--bs-el-color-primary', backgroundColor)
+    }
+  })
 }
