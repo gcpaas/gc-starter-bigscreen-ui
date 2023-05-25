0ce8978a3540bc5adaa92fe7fe42c47f7d095bf2
hong.yang <hong.yang@ustcinfo.com>
2023-05-25 17:18:33 +0800

    fix: 添加玫瑰图
    添加玫瑰图

---
+++ packages/G2Plots/饼图/玫瑰图.js
@@ -0,0 +1,129 @@
+
+// 分类
+const category = 'Rose'
+// 标题
+const title = '玫瑰图'
+// 类别， new Line()
+const chartType = 'Rose'
+// 用于标识，唯一，和文件夹名称一致
+const name = 'MeiGuiTu'
+
+// 右侧配置项
+const setting = [
+  {
+    label: '维度',
+    // 设置组件类型， select / input / colorPicker
+    type: 'select',
+    // 字段
+    field: 'xField',
+    // 对应options中的字段
+    optionField: 'xField',
+    // 是否多选
+    multiple: false,
+    value: '',
+    // tab页。 data: 数据， custom: 自定义
+    tabName: 'data'
+  },
+  {
+    label: '指标',
+    // 设置组件类型
+    type: 'select',
+    // 字段
+    field: 'yField',
+    // 对应options中的字段
+    optionField: 'yField',
+    // 是否多选
+    multiple: false,
+    value: '',
+    tabName: 'data'
+  },
+  {
+    label: '显示图例',
+    type: 'switch', // 设置组件类型
+    field: 'legendEnable', // 字段
+    optionField: 'legendEnable', // 对应options中的字段
+    value: false,
+    tabName: 'custom'
+  },
+  {
+    label: '图例位置',
+    type: 'select', // 设置组件类型
+    field: 'legendPosition', // 字段
+    optionField: 'legendPosition', // 对应options中的字段
+    // 是否多选
+    multiple: false,
+    value: 'top',
+    tabName: 'custom',
+    options: [
+      { label: '左上', value: 'top-left' },
+      { label: '顶部居中', value: 'top' },
+      { label: '右上', value: 'top-right' },
+      { label: '左侧', value: 'left' },
+      { label: '右侧', value: 'right' },
+      { label: '左下', value: 'bottom-left' },
+      { label: '底部', value: 'bottom' },
+      { label: '左下', value: 'bottom-right' }
+    ]
+  },
+  {
+    label: '颜色配置',
+    // 设置组件类型
+    type: 'colorSelect',
+    // 字段
+    field: 'color',
+    // 对应options中的字段
+    optionField: 'color',
+    value: ['#5B8FF9', '#61DDAA', '#5D7092', '#F6BD16', '#6F5EF9', '#6DC8EC', '#945FB9', '#FF9845', '#1E9493', '#FF99C3'],
+    tabName: 'custom'
+  },
+  {
+    label: '图表边距',
+    type: 'padding', // 设置组件类型
+    field: 'appendPadding', // 字段
+    optionField: 'appendPadding', // 对应options中的字段
+    value: [20, 20, 20, 20],
+    tabName: 'custom'
+  }
+]
+
+// 模拟数据
+const data = [
+  { type: '分类一', value: 27 },
+  { type: '分类二', value: 25 },
+  { type: '分类三', value: 18 },
+  { type: '分类四', value: 15 },
+  { type: '分类五', value: 10 },
+  { type: '其他', value: 5 }
+]
+
+// 数据处理脚本
+const dataHandler = '\n\n\n\n\n\n' +
+  'option.seriesField = option.xField;' +
+  '\noption.legend = option.legendEnable ? {position: setting.find(settingItem=>settingItem.field === \'legendPosition\').value} : false'
+
+
+// 图表配置 new Pie('domName', option)
+const option = {  data,
+  xField: 'type',
+  yField: 'value',
+  seriesField: 'type',
+  radius: 0.9,
+  color: ['#5B8FF9', '#61DDAA', '#65789B', '#F6BD16', '#7262fd', '#78d9eb', '#9661bc', '#f6903d', '#2f9e9a', '#f08aac'],
+  label: {
+    offset: -15,
+  },
+  legendEnable: true,
+  legendLayout: 'vertical',
+  legendPosition: 'top',
+  legend: false,
+  appendPadding: [20, 20, 20, 20], // 设置图标的边距
+}
+export default {
+  category,
+  title,
+  chartType,
+  name,
+  option,
+  setting,
+  dataHandler
+}
