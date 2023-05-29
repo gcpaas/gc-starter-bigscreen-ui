aecbe6f597e5486063a6a645edc07a5c8aa2278a
liu.shiyi <liu.shiyi@ustcinfo.com>
2023-05-29 11:20:52 +0800

    feat:修改部分组件的渐变色样式

--- packages/G2Plots/折线图/基础折线图.js
+++ packages/G2Plots/折线图/基础折线图.js
@@ -66,7 +66,7 @@ const setting = [
     type: 'gradual', // 设置组件类型
     field: 'lineStyle_stroke', // 字段
     optionField: 'lineStyle.stroke', // 对应options中的字段
-    value: 'l(0) 0:#5F92F9  1:#5F92F9',
+    value: 'l(0) 0:#5F92F9 1:#62FF00',
     tabName: 'custom'
   },
   {
@@ -148,7 +148,7 @@ const option = {
   },
   lineStyle: {
     lineWidth: 2,
-    stroke: 'l(0) 0:#5F92F9  1:#5F92F9'
+    stroke: 'l(0) 0:#5F92F9 1:#62FF00'
   },
   yAxis: {
     grid: {
