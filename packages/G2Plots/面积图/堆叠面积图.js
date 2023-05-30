1c04f75cf1c09dff1bda4c0819aeed3c4567cea5
hong.yang <hong.yang@ustcinfo.com>
2023-05-30 10:34:34 +0800

    fix: 面积图组件优化，将配置处理逻辑迁移到配置处理脚本
    面积图组件优化，将配置处理逻辑迁移到配置处理脚本，

--- packages/G2Plots/面积图/堆叠面积图.js
+++ packages/G2Plots/面积图/堆叠面积图.js
@@ -314,8 +314,11 @@ const data = [
   }
 ]
 
+// 配置处理脚本
+const optionHandler = 'option.legend = option.legendEnable ? {position: setting.find(settingItem=>settingItem.field === \'legendPosition\').value} : false;'
+
 // 数据处理脚本
-const dataHandler = 'option.legend = option.legendEnable ? {position: setting.find(settingItem=>settingItem.field === \'legendPosition\').value} : false;'
+const dataHandler = ''
 
 // 图表配置 new Line('domName', option)
 const option = {
@@ -362,5 +365,6 @@ export default {
   name,
   option,
   setting,
+  optionHandler,
   dataHandler
 }
