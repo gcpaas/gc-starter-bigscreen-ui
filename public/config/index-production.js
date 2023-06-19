e93b91f77870f55a44758cad7557eb905ca972d1
liu.chengbiao <liu.chengbiao@ustcinfo.com>
2023-06-19 21:39:31 +0800

    清空仓库

--- public/config/index-production.js
+++
@@ -1,6 +0,0 @@
-window.ENV = 'production'
-var productionConfig = {
-  baseUrl: 'http://gcpaas.gccloud.com/bigScreenServer'
-}
-// 必须的
-window.CONFIG = configDeepMerge(window.CONFIG, productionConfig)
